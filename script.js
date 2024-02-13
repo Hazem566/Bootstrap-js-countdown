23f95fd7e7ab4d9ebb027f4df3856458e4127c72
HAZEM <haznmhmwd535@gmail.com>
2024-02-13 18:58:39 +0200

    Update project => Day one!

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "Novamber",
+  "December",
+];
+
+const weekDays = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+const cols = document.querySelectorAll(".col");
+const deadlineText = document.querySelector(".deadline__text");
+
 const daysElem = document.getElementById("days");
 const hoursElem = document.getElementById("hours");
 const minutesElem = document.getElementById("minutes");
@@ -7,6 +35,8 @@ const deadlineDone = document.getElementById("alert-done");
 const deadlineInvalid = document.getElementById("alert-deadline");
 const interAlert = document.getElementById("alert-inter");
 
+let minutesIn = document.getElementById("inMinutes");
+let hoursIn = document.getElementById("inHours");
 let daysIn = document.getElementById("inDays");
 let monthesIn = document.getElementById("inMonthes");
 let yearsIn = document.getElementById("inYears");
@@ -14,53 +44,67 @@ let yearsIn = document.getElementById("inYears");
 const deadlineBtn = document.getElementById("deadline__btn");
 let timeInterval;
 
+const observer = new IntersectionObserver((entris) => {
+  entris.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.style.opacity = "1";
+      entry.target.style.transform = "translate(0,0)";
+    }
+  });
+});
+cols.forEach((col) => {
+  observer.observe(col);
+});
+
 const timeFormat = (time, string) => {
-  return time == 1
+  return time === 1
     ? `<span class="fs-1">${time}</span> ${string}`
     : `<span class="fs-1">${time}</span> ${string}s`;
 };
 
+const displayDeadline = () => {
+  const interdYears = yearsIn.value;
+  const interdMonths = monthesIn.value;
+  const interdDays = daysIn.value;
+  const interdHours = hoursIn.value;
+  const interdMinutes = minutesIn.value;
 
-const startCountdown = () => {
-  let daysIntered = daysIn.value;
-  let monthesIntered = monthesIn.value;
-  let yearsIntered = yearsIn.value;
-  let countdownDateIntered = new Date(
-    yearsIntered,
-    monthesIntered - 1,
-    daysIntered
+  const deadlineDate = new Date(
+    interdYears,
+    interdMonths-1,
+    interdDays,
+    interdHours,
+    interdMinutes,
+    0
   );
 
-  let countdown = new Date(countdownDateIntered).getTime();
-  let now = new Date().getTime();
-  let diff = (countdown - now) / 1000;
+  const deadlineMinutes = deadlineDate.getMinutes();
+  const deadlineHour = deadlineDate.getHours();
+  const deadlineWeekDay = deadlineDate.getDay();
+  deadlineWeekDay = weekDays[deadlineWeekDay];
+  const deadlineMonthDay = deadlineDate.getDate();
+  const deadlineMonth = deadlineDate.getMonth();
+  deadlineMonth = months[deadlineMonth];
+  const deadlineYear = deadlineDate.getFullYear();
 
-  if(now > countdown) {
-    clearInterval(timeInterval);
-    deadlineDone.classList.remove('d-n');
-    setTimeout(()=>{deadlineDone.classList.add('d-n')},2000);
-  } else if ((yearsIntered.length > 4 || yearsIntered.length < 4) || 
-  monthesIntered.length > 2 || daysIntered.length > 2 || 
-  (yearsIntered=='' || monthesIntered=='' || daysIntered=='') ||
-  diff < 0) {
-    clearInterval(timeInterval);
-    deadlineInvalid.classList.remove('d-n');
-    setTimeout(() => {deadlineInvalid.classList.add('d-n')}, 2000);
-  } else {
-    let days = Math.floor( diff / (60*60*24) );
-    let hours = Math.floor( (diff % (60*60*24)) / (60*60) );
-    let minutes = Math.floor( (diff % (60*60)) / 60 );
-    let seconds = Math.floor(diff % 60);
-    daysElem.innerHTML = timeFormat(days, "day");
-    hoursElem.innerHTML = timeFormat(hours, "hour");
-    minutesElem.innerHTML = timeFormat(minutes, 'minute');
-    secondsElem.innerHTML = timeFormat(seconds, "second");
-  }
+  deadlineText.innerText = `
+      Deadline ends on ${deadlineWeekDay}, ${deadlineMonthDay} ${deadlineMonth} ${deadlineYear} ${deadlineHour}:${deadlineMinutes}
+  `;
 
+  const deadlineDateToCalc = deadlineDate.getTime();
+  const now = new Date().getTime();
+  const currentYear = now.getFullYear();
+  const currentMonth = now.getMonth()+1;
+  const currentDay = now.getDate();
+  const currentHour = now.getHours();
+  const currentMinutes = now.getMinutes();
+
+  // if (interdYears<currentYear||
+  //     interdMonths<currentMonth||
+  //     interdDays<currentDay) {
+
+  // }
 };
+// Deadline ends on weekday, day month year hours:minutes am
 
-deadlineBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-  startCountdown();
-  timeInterval = setInterval(startCountdown, 1000);
-});
+console.log(new Date().getHours());
